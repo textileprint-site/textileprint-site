396dcbe839ff4f209449c5aa67946ec052967d7b
Gor <gor@example.com>
2026-06-20 21:27:09 +0400

    Add sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
+```js
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://textileprint.site',
+  integrations: [sitemap()],
+});
+```
